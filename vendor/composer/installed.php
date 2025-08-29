e48bc3fdab7437663f42df7dd789dd1851af4608
Conventional Changelog Action <wunderbar@mailbox.org>
2025-08-29 20:43:58 +0000

    chore: update composer.lock after version bump

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'tearoom1/kirby-ftp-backup',
-        'pretty_version' => '1.0.6',
-        'version' => '1.0.6.0',
+        'pretty_version' => '1.0.7',
+        'version' => '1.0.7.0',
         'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'tearoom1/kirby-ftp-backup' => array(
-            'pretty_version' => '1.0.6',
-            'version' => '1.0.6.0',
+            'pretty_version' => '1.0.7',
+            'version' => '1.0.7.0',
             'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
